e1a11d7ba71e2cb835767b6887d651d810250660
Krishna Nigalye <krishna.nigalye@gmail.com>
2021-10-07 15:53:26 +0530

    Update nws18.config.js

--- nws18.config.js
+++ nws18.config.js
@@ -6,4 +6,4 @@ const configuration = {
     "https://images.news18.com/static_news18/pix/ibnhome/news18/images/default-300x200.jpg?impolicy=website&width=150&height=100",
 };
 
-export default configuration;
+windows.nw18config = configuration;
