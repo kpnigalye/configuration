e2a9a3504659a17e9c916c57b7030fc933b94a01
Krishna Nigalye <krishna.nigalye@gmail.com>
2021-10-07 14:12:27 +0530

    sample configuration file

---
+++ nws18.config.js
@@ -0,0 +1,9 @@
+const configuration = {
+  merchantId: "130926",
+  articleLimit: 2,
+  secret: "keygen",
+  defaultImageUrl:
+    "https://images.news18.com/static_news18/pix/ibnhome/news18/images/default-300x200.jpg?impolicy=website&width=150&height=100",
+};
+
+export default configuration;
