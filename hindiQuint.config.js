daf29a52b7c8695113a470efe6c5755ba7cef1aa
Krishna Nigalye <krishna.nigalye@gmail.com>
2021-10-20 12:15:32 +0530

    Create hindiQuint.config.js

---
+++ hindiQuint.config.js
@@ -0,0 +1,11 @@
+const configuration = {
+  domain: "https://alfred.wru.ai",
+  merchantId: "130918",
+  strategy:'qwwidget',
+  articleLimit: 2,
+  secret: "keygen",
+  defaultImageUrl:
+    "https://images.news18.com/static_news18/pix/ibnhome/news18/images/default-300x200.jpg?impolicy=website&width=150&height=100",
+};
+
+window.wruConfig = configuration;
