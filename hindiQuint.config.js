e508fe46751ed9edde44c88857fe8a99e40c3e05
Krishna Nigalye <krishna.nigalye@gmail.com>
2021-10-26 13:29:45 +0530

    Update hindiQuint.config.js

--- hindiQuint.config.js
+++ hindiQuint.config.js
@@ -2,7 +2,7 @@ const configuration = {
   domain: "https://alfred.wru.ai",
   merchantId: "130918",
   strategy:'qwwidget',
-  articleLimit: 2,
+  articleLimit: 4,
   secret: "keygen",
   defaultImageUrl:
     "https://images.news18.com/static_news18/pix/ibnhome/news18/images/default-300x200.jpg?impolicy=website&width=150&height=100",
