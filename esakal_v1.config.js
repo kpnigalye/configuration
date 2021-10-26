628409627995859cddca80e9b89e694fb789814c
Krishna Nigalye <krishna.nigalye@gmail.com>
2021-10-26 15:10:42 +0530

    Update and rename esakal.config.js to esakal_v1.config.js

--- esakal_v1.config.js
+++ esakal_v1.config.js
@@ -3,7 +3,13 @@ const configuration = {
   merchantId: "130927",
   strategy:'content_similarity',
   articleLimit: 10,
+  amp : {
+    strategy:'content_similarity',
+    articleLimit: 5,
+  },
   secret: "keygen",
   defaultImageUrl:
     "https://images.news18.com/static_news18/pix/ibnhome/news18/images/default-300x200.jpg?impolicy=website&width=150&height=100",
 };
+
+window.wruConfig = configuration;
