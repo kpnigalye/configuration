ed2bddd71aa5cd192aa67ebc3870c1b07b3be7ef
Krishna Nigalye <krishna.nigalye@gmail.com>
2021-10-26 13:59:04 +0530

    Update esakal.config.js

--- esakal.config.js
+++ esakal.config.js
@@ -3,11 +3,6 @@ const configuration = {
   merchantId: "130927",
   strategy:'content_similarity',
   articleLimit: 10,
-  // amp-specific configuaration
-  amp: {
-    strategy:'content_similarity',
-    articleLimit: 5,    
-  },
   secret: "keygen",
   defaultImageUrl:
     "https://images.news18.com/static_news18/pix/ibnhome/news18/images/default-300x200.jpg?impolicy=website&width=150&height=100",
