6cc668cafbdf5debfb06ed64cf424d8007aa28a3
Krishna Nigalye <krishna.nigalye@gmail.com>
2021-11-08 17:25:32 +0530

    Update and rename quint.config.js to quint.configv1.js

--- quint.configv1.js
+++ quint.configv1.js
@@ -1,11 +1,14 @@
 const configuration = {
   domain: "https://alfred.wru.ai",
+  merchant: "quint",
   merchantId: "130915",
   strategy:'qwwidget',
   articleLimit: 4,
   secret: "keygen",
-  defaultImageUrl:
+  fallBackImageUrl:
     "https://images.news18.com/static_news18/pix/ibnhome/news18/images/default-300x200.jpg?impolicy=website&width=150&height=100",
+  customFields: [],
+  imgDomain: "https://images.thequint.com/",
 };
 
 window.wruConfig = configuration;
