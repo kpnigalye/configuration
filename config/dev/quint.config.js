44666eb29bd88bb5593734cb7b287e6769c3941e
Krishna Nigalye <krishna.nigalye@gmail.com>
2022-03-29 13:19:47 +0530

    Update quint.config.js

--- config/dev/quint.config.js
+++ config/dev/quint.config.js
@@ -11,7 +11,7 @@ const configuration = {
     "https://images.thequint.com/thequint/2021-06/b02e0cf0-8e1f-46e7-b75f-eac28f2dae78/quint_hero_light.svg",
   imgDomain: "https://images.thequint.com/",
   excludeArticlePublishDate: true,
-  stylesheet: "https://cdn.jsdelivr.net/gh/kpnigalye/configuration/css/quint-wru-widget.css",
+  stylesheet: "https://cdn.jsdelivr.net/gh/kpnigalye/configuration/css/quint/quint-wru-widget.css",
   disableWruAnalytics: true,
 };
 
