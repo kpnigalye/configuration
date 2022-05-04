27c67874de26827376bd653558d321a5f7b7c121
Sajid <sajidansari18@email.com>
2022-05-04 14:01:19 +0530

    test51

---
+++ widget/dev/test51/359.wru-widget.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwru_widget_react=self.webpackChunkwru_widget_react||[]).push([[359],{359:function(e,t,r){r.r(t);var a=r(294);r(140),t.default=function(e){var t=e.title;return a.createElement("div",{className:"wru-widget-header"},a.createElement("span",null,t))}}}]);
